57cc0be46d192321342d78ebc70f2b0a9add1fc0
ErivaldoCapitulino <vadositi@gmail.com>
2023-07-20 23:10:48 -0300

    retirando duplicação de código do setAttribute

--- aula04/acessibilidade/script.js
+++ aula04/acessibilidade/script.js
@@ -2,7 +2,7 @@ const btnEnviar = document.getElementById("btnEnviar");
 
 function changeAriaLabel(newLabel) {
   btnEnviar.setAttribute("aria-label", newLabel);
-  setAttribute("aria-label", newLabel);
+  // setAttribute("aria-label", newLabel);
 }
 
 btnEnviar.addEventListener("click", function () {
